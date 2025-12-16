0515731c21bac380b63b5a4c9807b772670efc84
Deva <devaydv18@gmail.com>
2025-12-16 14:25:41 +0530

    mongodb

--- ExpressBasics/mongodb/db/index.js
+++ ExpressBasics/mongodb/db/index.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://devaydv18_db_user:PY9wKT1AkHJoa6Jn@cluster0.ztrd28u.mongodb.net/course_selling_app')
-// mongoose.connect('mongodb+srv://devaydv18_db_user:<PY9wKT1AkHJoa6Jn>@cluster0.ztrd28u.mongodb.net/course_selling_app')
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URL)
 
 const AdminSchema = new mongoose.Schema({
     username: String,
     password: String
-    
 })
+
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    purchasedCourses:[{
+    purchasedCourses: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Course'
     }]
 })
+
 const CourseSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -22,11 +24,8 @@ const CourseSchema = new mongoose.Schema({
     price: Number
 })
 
+const Admin = mongoose.model('Admin', AdminSchema)
+const User = mongoose.model('User', UserSchema)
+const Course = mongoose.model('Course', CourseSchema)
 
-const Admin = mongoose.model('Admin',AdminSchema)
-const User = mongoose.model('User',UserSchema)
-const Course = mongoose.model('Course',CourseSchema)
-
-module.exports = {
-    Admin, User, Course
-}
+module.exports = { Admin, User, Course }
